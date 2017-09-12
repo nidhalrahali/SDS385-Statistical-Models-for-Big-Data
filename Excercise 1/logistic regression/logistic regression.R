7f9ac9d5dcbbfd122ca36bbecef64fe144f0507f
Yinan Zhu <welltempered.ivan@gmail.com>
2017-09-12 00:55:06 -0500

    update

--- Excercise 1/logistic regression/logistic regression.R
+++ Excercise 1/logistic regression/logistic regression.R
@@ -1,8 +1,19 @@
 library(readr)
 data <- read_csv("~/GitHub/SDS385-course-work/Excercise 1/logistic regression/wdbc.csv",col_names = FALSE)
+source('~/GitHub/SDS385-course-work/Excercise 1/logistic regression/gradient decent.R')
 X=as.matrix(data[3:12])
+for(i in 1:ncol(X)){
+  X[,i]=(X[,i]-mean(X[,i]))/sqrt(var(X[,i]))
+}
+X=cbind(X,1)
 y=as.vector(matrix(nrow=nrow(data),ncol=1))
 for(i in 1:nrow(data)){
   if(data[i,2]=="M")y[i]=1
   else y[i]=0
 }
+beta0=as.vector(matrix(0.1,nrow=11))
+ite=1000
+eps=0.0001
+result=gradientdecent(X,y,beta0,eps,ite)
+plot(result$negloglikelihood)
+result$beta
