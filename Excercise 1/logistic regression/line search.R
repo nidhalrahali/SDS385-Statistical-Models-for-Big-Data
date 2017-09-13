4045902dbbd09030eca21b4150e4cdf0c0bd042a
Yinan Zhu <welltempered.ivan@gmail.com>
2017-09-12 23:57:30 -0500

    update

--- Excercise 1/logistic regression/line search.R
+++ Excercise 1/logistic regression/line search.R
@@ -6,6 +6,35 @@ nllh=function(omega,y){
   return=-sum(log((2*y-1)*omega+1-y))
 }
 
+wolfcondition=function(eps, beta, g,y,X,c1,c2){
+  omega=as.vector(1/(exp(-X%*%beta)+1))
+  nllhoriginal=nllh(omega,y)
+  betatrial=beta+eps*g
+  omega=as.vector(1/(exp(-X%*%betatrial)+1))
+  nllhtrial=nllh(omega,y)
+  gtrial=grad(omega,y,X)
+  if(nllhtrial<nllhoriginal+c1*eps*crossprod(g,beta) & abs(crossprod(gtrial,beta))<c2*abs(crossprod(g,beta)))return=TRUE
+  else return=FALSE
+}
+
+gradientdecent_varyingstep=function(X,y,beta0,ite,c1,c2){
+  beta=beta0
+  nllh=as.vector(matrix(nrow=ite))
+  accepteps=as.vector(matrix(nrow=ite))
+  for(i in 1:ite){
+    omega=as.vector(1/(exp(-X%*%beta)+1))
+    nllh[i]=nllh(omega,y)
+    g=grad(omega,y,X)
+    for(j in 1:1000){
+      eps=j/5000
+      if(wolfcondition(eps,beta,g,y,X,c1,c2))break
+    }
+    accepteps[i]=eps
+    beta=beta+eps*g
+  }
+  return=list(beta=beta,negloglikelihood=nllh,step=accepteps)
+}
+
 gradientdecent=function(X,y,beta0,eps,ite){
   beta=beta0
   nllh=as.vector(matrix(nrow=ite))
--- Excercise 1/logistic regression/logistic regression.R
+++ Excercise 1/logistic regression/logistic regression.R
@@ -11,14 +11,23 @@ for(i in 1:nrow(data)){
   if(data[i,2]=="M")y[i]=1
   else y[i]=0
 }
-beta0=as.vector(matrix(0.1,nrow=11))
-ite=10000
-eps=0.02
-resultgd=gradientdecent(X,y,beta0,eps,ite)
-plot(resultgd$negloglikelihood)
-resultgd$beta
+beta0=as.vector(matrix(1,nrow=11))
+ite=1000
+eps=0.01
+result1=gradientdecent(X,y,beta0,eps,ite)
+plot(result1$negloglikelihood)
+result1$negloglikelihood
+result1$beta
 
 ite=20
-resultnt=newtonmethod(X,y,beta0,ite)
-resultnt$beta
-plot(resultnt$negloglikelihood)
+beta0=as.vector(matrix(0.7,nrow=11))
+result2=newtonmethod(X,y,beta0,ite)
+result2$beta
+plot(result2$negloglikelihood)
+result2$negloglikelihood
+
+result3=gradientdecent_varyingstep(X,y,beta0,ite,0.7,0.9)
+plot(result3$negloglikelihood)
+result3$negloglikelihood
+result3$beta
+result3$step
