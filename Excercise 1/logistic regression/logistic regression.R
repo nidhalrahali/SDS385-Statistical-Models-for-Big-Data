fffc351db78c42ae6b9fdd0a6a73d5dbffbe96fb
yinanzhu12 <welltempered.ivan@gmail.com>
2017-09-15 21:04:45 -0500

    update

--- Excercise 1/logistic regression/logistic regression.R
+++ Excercise 1/logistic regression/logistic regression.R
@@ -12,19 +12,29 @@ for(i in 1:nrow(data)){
   else y[i]=0
 }
 beta0=as.vector(matrix(1,nrow=11))
+
+trainX=X[1:250,]
+trainy=y[1:250]
+
 ite=1000
 eps=0.01
-result1=gradientdecent(X,y,beta0,eps,ite)
+result1=gradientdecent(trainX,trainy,beta0,eps,ite)
 plot(result1$negloglikelihood)
 result1$negloglikelihood
 result1$beta
 
+testX=X[251:569,]
+testy=y[251:569]
+likelihood=-nllh(1/(exp(-as.vector(testX%*%result1$beta))+1),testy)
+
+
 ite=20
 beta0=as.vector(matrix(0.7,nrow=11))
-result2=newtonmethod(X,y,beta0,ite)
+result2=newtonmethod(trainX,trainy,beta0,ite)
 result2$beta
 plot(result2$negloglikelihood)
 result2$negloglikelihood
+likelihood=-nllh(1/(exp(-as.vector(testX%*%result2$beta))+1),testy)
 
 result3=gradientdecent_varyingstep(X,y,beta0,ite,0.7,0.9)
 plot(result3$negloglikelihood)
