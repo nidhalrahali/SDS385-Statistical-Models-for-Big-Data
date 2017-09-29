c59d2a6d7f92da62c8ebdc7f20d64bd83cdb391d
Yinan Zhu <welltempered.ivan@gmail.com>
2017-09-29 00:48:39 -0500

    update

--- Excercise 1/linear equation solver/linear solver.R
+++ Excercise 1/linear equation solver/linear solver.R
@@ -1,38 +1,23 @@
 library(Matrix)
-linearsolve1=function(y,X){
-  m=X
-  m=crossprod(X,m)
+linearsolve=function(y,X){
+  m=crossprod(X,X)
   b=crossprod(X,y)
   return=solve(m)%*%b
 }
 
-linearsolve2=function(y,X){
-  m=X
-  m=crossprod(X,m)
+linearsolvelu=function(y,X){
+  m=crossprod(X,X)
   b=crossprod(X,y)
   lufact=expand(lu(m))
-  l=as.matrix(lufact$L)
-  u=as.matrix(lufact$U)
-  p=lufact$P
-  b=p%*%b
-  for(i in 1:nrow(l)){
-    db=b[i]/l[i,i]*as.vector(l[,i])
-    db[i]=0
-    b=b-db
-    b[i]=b[i]/l[i,i]
-  }
-  for(i in rev(1:nrow(u))){
-    db=b[i]/u[i,i]*as.vector(u[,i])
-    db[i]=0
-    b=b-db
-    b[i]=b[i]/u[i,i]
-  }
+  b=lufact$P%*%b
+  b=solve(lufact$L,b)
+  b=solve(lufact$U,b)
   return=b
 }
 
-linearsolves=function(y,X){
+linearsolvesparse=function(y,X){
   m=as(X,"sparseMatrix")
   m=crossprod(m,m)
   b=crossprod(X,y)
-  return=solve(m,b)
+  return=solve(m,b,sparse=TRUE)
 }
