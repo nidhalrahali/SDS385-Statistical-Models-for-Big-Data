c466242f9507ecbad2cebf0ab32e0671e2b9ad7d
yinanzhu12 <welltempered.ivan@gmail.com>
2017-10-22 20:47:50 -0500

    update

---
+++ Excercise 6/proximal gradient decent/functions.R
@@ -0,0 +1,28 @@
+target_function=function(X,Y,beta,lambda){
+  mean((Y-X%*%beta)^2)+lambda*sum(abs(beta))
+}
+
+soft_threshold=function(y,lambda){
+  ret=rep(0,length(y))
+  for(i in 1:length(y)){
+    ret[i] = sign(y[i])*max(c(0,abs(y[i])-lambda))
+  }
+  ret
+}
+
+gradient=function(X,Y,beta){
+  crossprod(X,X%*%beta-Y)/length(Y)
+}
+
+
+prox_gd=function(X,Y,beta0,lambda,gamma,ite){
+  beta=beta0
+  tf_history=rep(0,ite)
+  for(it in 1:ite){
+    grad=gradient(X,Y,beta)
+    u=beta-gamma*grad
+    beta=soft_threshold(u,lambda*gamma)
+    tf_history[it]=target_function(X,Y,beta,lambda)
+  }
+  list(beta=beta,targetfunction=tf_history)
+}
