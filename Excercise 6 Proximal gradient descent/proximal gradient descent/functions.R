a2feaee08fbccf9dd2025c114333aa68b2085f12
yinanzhu12 <welltempered.ivan@gmail.com>
2017-10-29 14:57:02 -0500

    update

--- Excercise 6 Proximal gradient descent/proximal gradient descent/functions.R
+++ Excercise 6 Proximal gradient descent/proximal gradient descent/functions.R
@@ -26,3 +26,36 @@ prox_gd=function(X,Y,beta0,lambda,gamma,ite){
   }
   list(beta=beta,targetfunction=tf_history)
 }
+
+acc_prox_gd=function(X,Y,beta0,lambda,gamma,ite){
+  tf_history=rep(0,ite)
+  s1=10
+  z=beta0
+  beta1=beta0
+  for(it in 1:ite){
+    grad=gradient(X,Y,z)
+    u=z-gamma*grad
+    beta2=soft_threshold(u,lambda*gamma)
+    tf_history[it]=target_function(X,Y,beta2,lambda)
+    s2=(1+sqrt(1+4*s1^2))/2
+    z=beta2+(s1-2)/s2*(beta2-beta1)
+    beta1=beta2
+    s1=s2
+  }
+  list(beta=beta2,targetfunction=tf_history)
+}
+
+lazy_acc_prox_gd=function(X,Y,beta0,lambda,gamma,ite,s){
+  tf_history=rep(0,ite)
+  z=beta0
+  beta1=beta0
+  for(it in 1:ite){
+    grad=gradient(X,Y,z)
+    u=z-gamma*grad
+    beta2=soft_threshold(u,lambda*gamma)
+    tf_history[it]=target_function(X,Y,beta2,lambda)
+    z=beta2+s*(beta2-beta1)
+    beta1=beta2
+  }
+  list(beta=beta2,targetfunction=tf_history)
+}
