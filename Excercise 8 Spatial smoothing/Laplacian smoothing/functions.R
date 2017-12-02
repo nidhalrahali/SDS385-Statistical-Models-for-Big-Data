0735b01016e3605b2a14de291b81786c45c9e115
yinanzhu12 <welltempered.ivan@gmail.com>
2017-12-02 16:27:34 -0600

    update

--- Excercise 8 Spatial smoothing/Laplacian smoothing/functions.R
+++ Excercise 8 Spatial smoothing/Laplacian smoothing/functions.R
@@ -9,3 +9,33 @@ makeC_sparse = function (dim1, dim2,lambda)  {
   D=rBind(D1,D2)
   crossprod(D,D)*lambda+bandSparse(n,k=0,diagonals=list(rep(1,n)))
 }
+
+gauss_seidel=function(C,b,ite,targetfunction=FALSE){
+  L=tril(C)
+  U=C-L
+  n=length(b)
+  x=solve(L,b,sparse=TRUE)
+  tf=rep(0,ite)
+  for (i in 1:ite){
+    x=solve(L,b-U%*%x,sparse=TRUE)
+    if(targetfunction==TRUE)tf[i]=target_function(C,y,x)
+  }
+  list(x,tf)
+}
+jacobi=function(C,b,ite,targetfunction=FALSE){
+  R=C-band(C,0,0)
+  D=diag(C)
+  n=length(b)
+  bD=b/D
+  x=bD
+  RD=R/D
+  tf=rep(0,ite)
+  for (i in 1:ite){
+    x=bD-RD%*%x
+    if(targetfunction==TRUE)tf[i]=target_function(C,y,x)
+  }
+  list(x,tf)
+}
+target_function=function(C,y,x){
+  crossprod(x,C%*%x)/2-crossprod(y,x)
+}
