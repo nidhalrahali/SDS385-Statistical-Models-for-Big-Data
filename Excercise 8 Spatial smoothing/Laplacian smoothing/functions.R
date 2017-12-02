d342d93c33d8d5fd831d1feaac5d8dcd310954a9
yinanzhu12 <welltempered.ivan@gmail.com>
2017-12-01 20:58:44 -0600

    update

---
+++ Excercise 8 Spatial smoothing/Laplacian smoothing/functions.R
@@ -0,0 +1,12 @@
+library(Matrix)
+
+makeD2_sparse = function (dim1, dim2)  {
+  D1 = bandSparse(dim1 * dim2, m = dim1 * dim2, k = c(0, 1), 
+                  diagonals = list(rep(-1, dim1 * dim2), rep(1, dim1 * 
+                                                               dim2 - 1)))
+  D1 = D1[(seq(1, dim1 * dim2)%%dim1) != 0, ]
+  D2 = bandSparse(dim1 * dim2 - dim1, m = dim1 * dim2, k = c(0, 
+                                                             dim1), diagonals = list(rep(-1, dim1 * dim2), rep(1, 
+                                                                                                               dim1 * dim2 - 1)))
+  return(rBind(D1, D2))
+}
